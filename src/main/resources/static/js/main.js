393834000be26e14cc9a4a263647c8dbd1699616
JPWon-1 <62930150+JPWon-1@users.noreply.github.com>
2021-06-26 19:48:30 +0900

    modify : 포트폴리오 구성 고민중.....

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,6 +1,8 @@
-// window.onscroll = function () {
-//     console.log('scrolling');
-// };
-document.getElementById("about_me").onscroll = function () {
-    console.log('scrolling');
-};
+var profileImg = document.getElementById("profile_img")
+var aboutMe = document.getElementById("about_me")
+window.onscroll = function () {
+    let value = window.pageYOffset / aboutMe.offsetTop + 1;
+    if(value>1.34){return false}
+    profileImg.style.transform = `scale(${value})`;
+}
+
