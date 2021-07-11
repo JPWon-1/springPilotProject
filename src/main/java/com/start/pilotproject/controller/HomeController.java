bfca74e05c750000ba968a2440afc26815f4d1a0
JPWon-1 <62930150+JPWon-1@users.noreply.github.com>
2021-07-11 12:38:18 +0900

    feat : 임의의 데이터를 넣어서 thymeleaf에 뿌려주는 것 까지 해봄

--- src/main/java/com/start/pilotproject/controller/HomeController.java
+++ src/main/java/com/start/pilotproject/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com.start.pilotproject.controller;
 
+import com.start.pilotproject.repository.history.HistoryRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
+    @Autowired
+    HistoryRepository historyRepository;
+
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("historyList",historyRepository.findAll());
         return "main";
     }
    
