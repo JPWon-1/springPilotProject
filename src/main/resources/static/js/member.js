78df8f07bee282d742fafd037d6ca2679945bc4a
JPWon-1 <62930150+JPWon-1@users.noreply.github.com>
2021-09-03 00:50:28 +0900

    modify path

---
+++ src/main/resources/static/js/member.js
@@ -0,0 +1,53 @@
+var member = {
+    init: function () {
+        var _this = this;
+        // document.getElementById("signUp").onclick = function(){
+        //     _this.save();
+        // };
+        document.getElementById("loginBtn").onclick = function(){
+            _this.login();
+        };
+    },
+    save: function () {
+        var data = {
+            email: $("#email").val(),
+            password: $("#password").val(),
+        };
+        $.ajax({
+            type: "POST",
+            url: "/signUp",
+            contentType: "application/json;charset=utf-8",
+            data: JSON.stringify(data),
+        }).done(function (response,textStatus,xhr) {
+            console.log(response);
+            console.log(textStatus);
+            alert("회원가입이 완료되었습니다.")
+        }).fail(function (response) {
+            alert(response.responseJSON.message);
+        });
+    },
+    login: function (){
+        var data = {
+            email: $("#email").val(),
+            password: $("#password").val(),
+        };
+        $.ajax({
+            type: "POST",
+            url: "/login",
+            // dataType: "json",
+            contentType: "application/json;charset=utf-8",
+            data: JSON.stringify(data),
+        }).done(function (response,textStatus,xhr) {
+            console.log(response);
+            console.log(textStatus);
+            alert("로그인 되었습니다.")
+
+            localStorage.setItem('token',response.Authorization);
+
+
+        }).fail(function (response) {
+            alert(response.responseJSON.message);
+        });
+    }
+};
+member.init();
